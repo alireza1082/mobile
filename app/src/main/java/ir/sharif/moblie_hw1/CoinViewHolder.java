2927c63406bf7ab58dd284b0c85c1b2f76525a0f
alireza <rezaali1387@gmail.com>
2021-03-07 18:42:33 +0330

    added CoinViewHolder.java

--- app/src/main/java/ir/sharif/moblie_hw1/CoinViewHolder.java
+++ app/src/main/java/ir/sharif/moblie_hw1/CoinViewHolder.java
@@ -1,4 +1,42 @@
 package ir.sharif.moblie_hw1;
 
-public class CoinViewHolder {
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CoinViewHolder extends RecyclerView.ViewHolder {
+    private ImageView coin_Image;
+    private TextView coin_Name;
+    private TextView coin_Fee;
+    private TextView Hour;
+    private TextView day;
+    private TextView week;
+
+
+    public CoinViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+
+    public void setCoin_Image(ImageView coin_Image) {
+        this.coin_Image = coin_Image;
+    }
+
+    public void setCoin_Fee(TextView coin_Fee) {
+        this.coin_Fee = coin_Fee;
+    }
+
+    public void setHour(TextView hour) {
+        Hour = hour;
+    }
+
+    public void setDay(TextView day) {
+        this.day = day;
+    }
+
+    public void setWeek(TextView week) {
+        this.week = week;
+    }
 }
