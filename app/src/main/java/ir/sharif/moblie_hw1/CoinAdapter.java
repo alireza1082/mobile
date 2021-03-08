9fbd4b82ffc03bd0ed2e185de081235ff2662ba5
alireza <rezaali1387@gmail.com>
2021-03-08 07:09:04 +0330

    added CoinAdapter

---
+++ app/src/main/java/ir/sharif/moblie_hw1/CoinAdapter.java
@@ -0,0 +1,24 @@
+package ir.sharif.moblie_hw1;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CoinAdapter extends RecyclerView.Adapter<CoinViewHolder> {
+    @NonNull
+    @Override
+    public CoinViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CoinViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
